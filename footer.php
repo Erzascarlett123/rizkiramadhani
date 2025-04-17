dccbe5780a567e9d896e736d50f9e350bcfc9e29
erza <satriaseptian724@gmail.com>
2025-04-17 11:37:03 +0700

    first

---
+++ footer.php
@@ -0,0 +1,28 @@
+</div>
+
+<div class="mt-3 p-4" style="background: #008080;">
+	<div class="container">	
+		<div class="row">
+			<div class="col-md-6 col-6 mt-2">
+				<img src="assets/file/zeni.png" width="70"> <br><br>
+				<span class=" mb-md-0 text-white">&copy; Pusat Pendidikan Zeni 2025</span>
+			</div>
+			<div class="col-md-6 col-6 text-end">
+				<a href="https://www.instagram.com/pusat_pendidikan_zeni?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" class="text-decoration-none text-white">
+					<span class="bi bi-instagram mx-3 fs-5"></span>
+				</a>
+				<a href="www.youtube.com/@pusdikzi_official4259" class="text-decoration-none text-white">
+					<span class="bi bi-youtube mx-3 fs-5"></span>
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+	AOS.init();
+</script>
+<script type="text/javascript" src="assets/js/bootstrap.bundle.js"></script>
+</body>
+</html>
---
+++ tentang_kami.php
@@ -0,0 +1,53 @@
+<?php 
+include "header.php";
+?>
+<div class="container">
+	<div class="row my-5">
+		<div class="col-md-4" data-aos="fade-right">
+			<img src="assets/file/sudirman.jpeg" style="width: 100%;">
+		</div>
+		<div class="col-md-6 offset-1" style="text-align: justify;" data-aos="fade-left">
+			<h3>Sejarah Pusat Pendidikan Zeni</h3>
+			<p>
+			Jejak Perjalanan Kesatria Konstruksi Indonesia" adalah sebuah karya yang mempersembahkan perjalanan epik dari titik awal hingga kejayaan Pusat Pendidikan Zeni Tentara Nasional Indonesia Angkatan Darat. Melalui penelusuran yang mendalam, buku ini mengungkapkan asal mula megahnya bangunan PUSDIKZI TNI AD serta lahirnya Sekolah Genie Batujajar dan Sekolah Kader Genie Kleco/Ngendro Solo yang telah menjadi mercusuar keilmuan bagi para insinyur militer. Menyusuri jejak perjalanan dari Depot Pasukan Genie Bogor, buku ini menggambarkan transformasi dari Pusat Pendidikan Genie AD (PPGIAD) menjadi Resimen Zeni (RINZI), hingga akhirnya menjadi PUSDIKZI yang menjadi sentralnya keahlian konstruksi di Indonesia. Dengan kisah yang memukau, buku ini menonjolkan perjalanan heroik dan dedikasi para prajurit Zeni dalam menjaga keutuhan bangsa dan membangun masa depan yang lebih baik. Dari RINZI hingga PUSDIKZI, pembaca akan diajak merenungkan peran penting lembaga ini dalam membangun fondasi pertahanan negara yang tangguh dan memberikan pencerahan bagi generasi muda Indonesia. Melalui penugasan yang menantang, buku ini menggugah semangat patriotisme serta kebanggaan akan prestasi yang telah dicapai oleh Pusat Pendidikan Zeni, menawarkan inspirasi dan pelajaran berharga bagi siapa pun yang merindukan kejayaan dan kemajuan bangsa. Dengan gaya narasi yang mengalir dan penekanan pada detail-detail penting, "Sejarah Pusat Pendidikan Zeni: Jejak Perjalanan Kesatria Konstruksi Indonesia" menjanjikan sebuah perjalanan yang mendalam dan menggugah, memperlihatkan kekuatan yang muncul dari ketekunan, kerja keras, dan semangat juang yang tak pernah padam. <br><br>
+
+			</p>
+			<b>Pusdikzi</b>
+		</div>
+	</div>
+	<hr>
+	<div class="my-5" style="text-align: justify;" data-aos="fade-up">
+		<div class="text-center">
+			<h3>Visi & Misi</h3>
+		</div>
+		<div class="my-5" style="text-align: justify;" data-aos="fade-up">
+		<div class="text-center">
+			<h4>Visi</h4>
+		</div>
+
+<p>Menjadi lembaga pendidikan kebanggaan Korps Zeni TNI Angkatan Darat yang profesional, berdedikasi, modern, dan berwawasan teknologi dalam membentuk postur prajurit zeni yang berpikir, bersikap, dan bertindak terbaik bagi negara dan bangsa.</p>
+
+<div class="my-5" style="text-align: justify;" data-aos="fade-up">
+		<div class="text-center">
+			<h4>Misi</h4>
+		</div>
+
+<p>1. Menyelenggarakan pendidikan untuk mencetak prajurit zeni yang profesional di bidangnya.</p>
+<p>2. Mewujudkan Pusat Pendidikan Zeni TNI Angkatan Darat sebagai pusat pengembangan doktrin, ilmu pengetahuan, dan teknologi di bidang konstruksi, destruksi, penyeberangan, samaran, rintangan, perbekalan air listrik, lidikzi, jihandak, dan nubika.</p>
+<p>3. Mengembangkan kemampuan serta jiwa juang gadik dan gapendik dalam melaksanakan tugas pengajaran.</p>
+<p>4. Mewujudkan Pusat Pendidikan Zeni sebagai almamater kebanggaan prajurit zeni.  
+		</p>
+		<div class="text-center">
+	<hr>
+	<div class="my-5" style="text-align: justify;" data-aos="fade-up">
+	<div class="text-center">
+		<h4>Struktur Organisasi</h4>
+	</div>
+</div>
+<div class="text-center my-4" data-aos="fade-up">
+		<img src="assets/file/Picture1.jpg" alt="Struktur Organisasi" style="width: 80%; max-width: 800px;">
+	</div>
+</div>
+<?php 
+include "footer.php";
+?>
